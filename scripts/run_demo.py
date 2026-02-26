07213e67d239ee34833aefd737d81510e164318f
sjaenich <jaenich@gmail.com>
2026-02-26 14:33:57 +0000

    added source dir instead of files

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -12,11 +12,11 @@ from pipeline.experiment import ExperimentRunner
 def main():
     project = Project(
         name="libcurl",
-        source_dir=Path("/path/to/project"),
+        source_dir=Path("/workspaces/RevEng/buildroot-2025.02.4/output/build/libcurl-7.71.1/lib"),
         build_dir=Path("/workspaces/RevEng/buildroot-2025.02.4/"),
         include_dir=Path("/workspaces/RevEng/buildroot-2025.02.4/output/build/libcurl-7.71.1/lib"),
     )
-
+    print("*** Running experiment for project:", project.name, "***")
     runner = ExperimentRunner(
         build_manager=BuildrootBuildManager(project.build_dir, project.build_dir),
         locator=ConfigLocator(),
@@ -24,7 +24,7 @@ def main():
         truth_extractor=GroundTruthExtractor(),
         comparator=ResultComparator(),
     )
-
+    print("Running experiment...")
     result = runner.run_project(project)
     print(result)
 
